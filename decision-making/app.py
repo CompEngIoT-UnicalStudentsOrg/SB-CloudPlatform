516b17283a97f2125d44aab441157788f8e6cd4b
mar <gim.marcello@gmail.com>
2021-06-20 18:08:06 +0200

    https://github.com/CompEngIoT-UnicalStudentsOrg/SB-CloudPlatform/issues/8 - Decision making integration
    - decision-making/app.py: added room information and functionalities.
    - services/flows.json: fixed decision making integration.

--- decision-making/app.py
+++ decision-making/app.py
@@ -9,9 +9,25 @@ app = Flask(__name__)
 api = Api(app)
 
 
+def get_roomcap(room):
+	room_dimension = [140, 1200, 700, 200] # mq
+	return room_dimension[room - 1]
+
+
+@api.route("/api/peoplecap/<int:roomid>")
+class PeopleCap(Resource):
+	def get(self, roomid):
+		try:
+			return get_roomcap(roomid), 200
+		except:			
+			return {"error": "invalid room"}, 422
+
+
+
 @api.route("/api/decision")
 class DecisionMaker(Resource):
 	@use_args({
+		"room": fields.Int(required=True),
 		"temperature": fields.Float(required=True),
 		"humidity": fields.Float(required=True),
 		"summer": fields.Bool(required=True)
@@ -20,6 +36,7 @@ class DecisionMaker(Resource):
 		temp = float(args["temperature"])
 		hum = float(args["humidity"])
 		summer = bool(args["summer"])
+
 		cmd = {}
 		temp_max = 0
 		temp_min = 0
@@ -27,7 +44,7 @@ class DecisionMaker(Resource):
 		hum_min = 47
 		critical_max_hum = 65
 		critical_min_hum = 40
-		room_dimension = 200 # mq
+		
 		cmd = {
 			"heating": 0,
 			"ventilaton": 0,
@@ -68,7 +85,10 @@ class DecisionMaker(Resource):
 		else:
 			cmd["distance"] = 1.5
 
-		cmd["people_cap"] = abs(room_dimension//(cmd["distance"] + 0.2))
+		try:
+			cmd["people_cap"] = abs(get_roomcap(args["room"])//(cmd["distance"] + 0.2))
+		except:
+			return {"error": "invalid room"}, 422
 
 		return cmd, 200
 
